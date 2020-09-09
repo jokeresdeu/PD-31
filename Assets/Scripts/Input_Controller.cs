a2844cb72e67e583b066b554302ff24f8d326973
DESKTOP-7RQV0BS\joker <oles.dibrivniy@tellurionmobile.com>
2020-09-09 13:13:46 +0300

    second lesson

---
+++ Assets/Scripts/Input_Controller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Input_Controller : MonoBehaviour
+{
+    [SerializeField] private Player_Movement _playerMovement;
+    private float _move;
+    private bool _jump;
+    private bool _crawling;
+
+    private void FixedUpdate()
+    {
+        _playerMovement.Move(_move, _crawling, _jump);
+        _jump = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        _move = Input.GetAxisRaw("Horizontal");
+        if (Input.GetButtonUp("Jump"))
+        {
+            _jump = true;
+        }
+        _crawling = Input.GetKey(KeyCode.C);
+    }
+}
--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -6,40 +6,104 @@ using UnityEngine;
 public class Player_Movement : MonoBehaviour
 {
     private Rigidbody2D _playerRB;
-    private SpriteRenderer _playerSprite;
+
+    [Header("Horizontal move")]
     [SerializeField]private float speed;
+
+    [Header("Jumping")]
+    [SerializeField]private float _jumpPower;
+    [SerializeField] private float _radius;
+    [SerializeField] private bool _airControll;
+    [SerializeField] private Transform _groundCheck;
+    [SerializeField] private LayerMask _whatIsGround;
+    bool _canJump;
+    bool _secondJump;
+    bool _checkJumpRange;
+
+    [Header("Crawling")]
+    [SerializeField] private float _crawlingSpeed;
+    [SerializeField] private LayerMask _whatIsCell;
+    [SerializeField] private Collider2D _headCollider;
+    [SerializeField] private Transform _cellCheck;
+    bool _canStand;
+    float _jumpY;
+
     bool _faceRight = true;
+   
+    
 
     void Start()
     {
         _playerRB = GetComponent<Rigidbody2D>();
-        _playerSprite = GetComponent<SpriteRenderer>();
     }
-
 
     private void FixedUpdate()
     {
-        if (Input.GetAxisRaw("Horizontal") != 0)
+        if (Physics2D.OverlapCircle(_groundCheck.position, _radius, _whatIsGround))
         {
-            _playerRB.velocity = Vector2.right * speed * Input.GetAxisRaw("Horizontal");
+            _canJump = true;
+            _secondJump = false;
+            _checkJumpRange = false;
         }
         else
-            _playerRB.velocity = Vector2.zero;
+            _canJump = false;
 
-        if(Input.GetAxisRaw("Horizontal")< 0 && !_playerSprite.flipX)
+        _canStand = !Physics2D.OverlapCircle(_cellCheck.position, _radius, _whatIsCell);
+
+        if(transform.position.y - _jumpY < 0.5f && _checkJumpRange)
         {
-            _playerSprite.flipX = true;
-        }    
-        else if(Input.GetAxisRaw("Horizontal") > 0 && _playerSprite.flipX)
-        {
-            _playerSprite.flipX = false;
-           
+            _playerRB.velocity = new Vector2(_playerRB.velocity.x, 0);
+            _checkJumpRange = false;
         }
-    }
-   
 
-    void Update()
-    {
-        
+
     }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(_groundCheck.position, _radius);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(_cellCheck.position, _radius);
+    }
+
+    public void Move(float move,  bool crawling, bool jump)
+    {
+        if (move != 0 && (_canJump || _airControll))
+        {
+            _playerRB.velocity = new Vector2(speed * move, _playerRB.velocity.y);
+        }
+        else if (move == 0 && _canJump)
+            _playerRB.velocity = new Vector2(0, _playerRB.velocity.y);
+
+        if (_faceRight && move < 0)
+            Flip();
+        else if (!_faceRight && move > 0)
+            Flip();
+
+        if (jump && (_canJump || !_secondJump))
+        {
+            _secondJump = true;
+            _jumpY = transform.position.y;
+            _checkJumpRange = true;
+            _playerRB.AddForce(Vector2.up * _jumpPower);
+        }
+
+        if (crawling)
+        {
+            _headCollider.enabled = false;
+        }
+        else if (!crawling && _canStand)
+        {
+            _headCollider.enabled = true;
+        }
+
+    }
+
+    void Flip()
+    {
+        _faceRight = !_faceRight;
+        transform.Rotate(0, 180, 0);
+    }
+
+
 }
