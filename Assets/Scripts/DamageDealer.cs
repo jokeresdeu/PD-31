236c56d14cb823e5055d4102a955f2683e150ffe
DESKTOP-7RQV0BS\joker <oles.dibrivniy@tellurionmobile.com>
2020-10-06 21:18:14 +0300

    4th lesson

--- Assets/Scripts/DamageDealer.cs
+++ Assets/Scripts/DamageDealer.cs
@@ -5,38 +5,34 @@ using System;
 
 public class DamageDealer : MonoBehaviour
 {
-    [SerializeField] private float _timeStep;
     [SerializeField] private int _damage;
-    private DateTime _enconterTime;
-    PlayerController _player;
-    
+    [SerializeField] private float _timeDelay;
+    private Player_controller _player;
+    private DateTime _lastEncounter;
+
     private void OnTriggerEnter2D(Collider2D info)
     {
-        if((DateTime.Now - _enconterTime).TotalSeconds < 0.02f)
-        {
+        if ((DateTime.Now - _lastEncounter).TotalSeconds < _timeDelay/2)
             return;
-        }
-        _enconterTime = DateTime.Now;
-        _player = info.GetComponent<PlayerController>();
+
+        _lastEncounter = DateTime.Now;
+        _player = info.GetComponent<Player_controller>();
         if (_player != null)
-            _player.ChangeHP(-_damage);
+            _player.ChangeHp(-_damage);
     }
 
     private void OnTriggerExit2D(Collider2D info)
     {
-        
-        if (_player == info.GetComponent<PlayerController>())
-        {
-            _player = null;
-        }
+        if(_player == info.GetComponent<Player_controller>())
+            _player = null; 
     }
 
     private void Update()
     {
-        if(_player != null && (DateTime.Now - _enconterTime).TotalSeconds > _timeStep)
+        if(_player != null && (DateTime.Now - _lastEncounter).TotalSeconds > _timeDelay)
         {
-            _player.ChangeHP(-_damage);
-            _enconterTime = DateTime.Now;
+            _player.ChangeHp(-_damage);
+            _lastEncounter = DateTime.Now;
         }
     }
 }
